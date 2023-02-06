01172e3fa5105b7b66c6b81a7887bceb6c326947
joao garcia <jiboiadamor@gmail.com>
2023-02-06 15:26:59 +0000

    created validations for the models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+  has_secure_password
+
+  attr_accessor :password, :password_confirmation
+
+  validates :username, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true, length: { minimum: 5 }, numericality: { only_integer: true }, confirmation: true, on: :create
+  validates_confirmation_of :password
 end
